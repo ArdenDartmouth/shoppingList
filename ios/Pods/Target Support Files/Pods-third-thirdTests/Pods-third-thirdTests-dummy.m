4a71f06889c8cde539b46bf709ba6e0e5f91b4fd
Jinxu Guo <ardenguo@Jinxus-MacBook-Air.local>
2022-02-07 14:21:23 -0500

    files

--- ios/Pods/Target Support Files/Pods-third-thirdTests/Pods-third-thirdTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_third_thirdTests : NSObject
-@end
-@implementation PodsDummy_Pods_third_thirdTests
-@end
--- ios/Pods/Target Support Files/Pods-third/Pods-third-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_third : NSObject
-@end
-@implementation PodsDummy_Pods_third
-@end
